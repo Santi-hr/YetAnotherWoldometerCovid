09553767263d1b6f82bb795a9501a799cd1858ca
Santi-hr <san.2005@hotmail.com>
2020-04-10 20:56:45 +0200

    Fixed mainScript
    Testing code got uploaded by mistake

--- mainScript.py
+++ mainScript.py
@@ -14,7 +14,7 @@ logger = logging.getLogger("Main")
 while True:
     [next_datetime, next_delay] = timeHelper.get_next_daily_iteration(saves_per_day)
     logger.info("Next save at %s. Waiting %d seconds", next_datetime, next_delay)
-    # time.sleep(next_delay)
+    time.sleep(next_delay)
 
     [web_data, web_successful, web_datetime] = fetchWorldometer.fetch_worldometer(flag_save_raw_copy=True)
 
@@ -24,6 +24,5 @@ while True:
             logger.error("An error occurred while parsing the webpage")
         else:
             combineJsonWorldometerFcns.append_worldometer_into_output_json(dict_raw_data, web_datetime)
-    time.sleep(10)
 
 exit(1)
